605322d482b9af2629aa5fcb440cc5ade33d12cb
sarabjeet-ka <94822435+sarabjeet-ka@users.noreply.github.com>
2024-12-09 14:35:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "sarab-rg"
+    resource_group_name  = "aks-demo-rg"
     storage_account_name = "statefiles2428062000"            
     container_name       = "tfstate"                     
     key                  = "demo.terraform.tfstate"       
